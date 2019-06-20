3c0ad7ec2d8e1075e2332bc0db7c3d5f0c1030d0
Safin Timur <timurtlt96@mail.ru>
2019-06-20 13:17:35 +0300

    Edited method getting env var

--- logs_conf.py
+++ logs_conf.py
@@ -7,8 +7,8 @@ class LogsHandler(logging.Handler):
 
     def __init__(self):
         super().__init__()
-        self.bot = telegram.Bot(os.getenv('LOGGER_BOT_TOKEN'))
+        self.bot = telegram.Bot(os.environ.get('LOGGER_BOT_TOKEN'))
 
     def emit(self, record):
         log_entry = self.format(record)
-        self.bot.send_message(chat_id=os.getenv('LOGS_RECEIVER_ID'), text=log_entry)
+        self.bot.send_message(chat_id=os.environ.get('LOGS_RECEIVER_ID'), text=log_entry)
